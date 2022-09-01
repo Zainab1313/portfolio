68b6e0fc2a3982880484f8ee3f90739323c20c92
Zainab1313 <106315960+Zainab1313@users.noreply.github.com>
2022-09-01 10:49:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// function mouseOver() {
+//     document.getElementById("project1a").style.backgroundImage = "url(https://media.giphy.com/media/Z4K8wAKa15UZJiJhJ0/giphy.gif)";
+// }
+
+// function mouseOut() {
+//     document.getElementById("project1a").style.backgroundImage = "none";
+
+// }
